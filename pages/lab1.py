30aaf3a4237593ee47babb11ff16250c9bcff6ca
hjc <choehj0555@gmail.com>
2025-06-26 17:05:06 +0900

    add day3

--- pages/lab1.py
+++ pages/lab1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("lab1")
-url = "https://github.com/myoh0623/dataset/blob/main/student-mat.csv"
+url = "https://raw.githubusercontent.com/myoh0623/dataset/refs/heads/main/student-mat.csv"
 df = pd.read_csv(url)
 
 # 성별 평균 성적 비교(G3 컬럼은 성적을 나타낸다)
@@ -11,10 +11,15 @@ gender_score = df.groupby("sex")["G3"].mean()
 
 st.bar_chart(gender_score)
 
-# 공부 시간별 평균성ㅈ적을 bar chart로 study time 컬럼으로 group by
-st.subheader("2. 공부 시간별 평균 성적")
-study_score = df.groupby("studytime")["G3"].mean()
 
-# walc 주말알콜섭취 에 따른 평균 성적을 bar chart로
-st.subheader("2. 주말 알콜 섭취에 따른 평균 성적")
-walc_score = df.groupby("Walc")["G3"].mean()
+# 공부 시간별 평균 성적 을 bar chart로 studytime 컬럼으로 groupby 
+st.subheader("2. 공부 시간별 평균 최종 성적 (G3 컬럼)")
+study_time_avg = df.groupby("studytime")["G3"].mean()
+
+st.bar_chart(study_time_avg)
+
+# walc(주말 알콜 섭취) 에 따른 평균 성적 을 bar chart로
+st.subheader("3. Walc(주말 알콜 섭취) 에 따른 평균 성적")
+walc_time_avg = df.groupby("Walc")["G3"].mean()
+
+st.bar_chart(walc_time_avg)
